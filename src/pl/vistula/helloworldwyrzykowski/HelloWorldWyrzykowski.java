a0d51b44582c8951fa1c0d23b791339bc448baae
student <student@lab.pl.wsei>
2021-10-08 13:59:02 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldwyrzykowski/HelloWorldWyrzykowski.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldwyrzykowski;
+
+public class HelloWorldWyrzykowski {
+    public static void main(String[] args) {
+        System.out.println("HelloVistulaWyrzykowski");
+    }
+}
